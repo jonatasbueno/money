1689246edf7474414c1f871cc7c7d97eed1c9832
Jônatas Bueno <jonatas.livramento@gmail.com>
2023-05-04 01:24:57 -0300

    feat: Phosphor-react installed

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,7 +1,9 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function Trasactions() {
   return (<div>
     <Header />
+    <Summary />
   </div>)
 }
