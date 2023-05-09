0ac03d3a85d86f4397cff6b0d8423daf97f4d97d
Jônatas Bueno <jonatas.livramento@gmail.com>
2023-05-09 01:19:58 -0300

    feat: Intalled JSON SERVER lig

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
 import { Trasactions } from "./pages/Transactions";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
 
   return (
   <ThemeProvider theme={defaultTheme}>
     <GlobalStyle />
-
+      <TransactionsProvider>
       <Trasactions />
+      </TransactionsProvider>
   </ThemeProvider>
   )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,41 +1,42 @@
+import { useContext } from "react";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+
 import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
 
 export function Trasactions() {
-  return (<div>
-    <Header />
-    <Summary />
+  const { transactions } = useContext(TransactionsContext)
 
-    <TransactionContainer>
-      <SearchForm />
+  return (
+    <>
+      <Header />
+      <Summary />
 
-      <TransactionTable>
-        <tbody>
-          <tr>
-            <td width='50%'>Desensolvimento de sites</td>
-            <td>
-              <PriceHighlight variant="income">
-                R$ 12.000,00
-              </PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
+      <TransactionContainer>
+        <SearchForm />
 
-          <tr>
-            <td width='50%'>Desensolvimento de sites</td>
-            <td>
-              <PriceHighlight variant="outcome">
-                - R$ 50,00
-              </PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-        </tbody>
-      </TransactionTable>
-    </TransactionContainer>
-  </div>)
+        <TransactionTable>
+          <tbody>
+
+            {transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td width='50%'>{transaction.description}</td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
+
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+    </>
+  )
 }
